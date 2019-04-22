078227e2938e293439a3bcebf87876426cc8123c
Aleksi Kauppila <aleksi.kauppila@gmail.com>
2019-04-22 13:56:10 +0300

    Fix register route to use RegisterAction

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 
 return [
     ["POST", "/signin", \Auth\Controller\SignInAction::class],
-    ["POST", "/register", \Auth\Controller\SignInAction::class]
+    ["POST", "/register", \Auth\Controller\RegisterAction::class]
 ];
