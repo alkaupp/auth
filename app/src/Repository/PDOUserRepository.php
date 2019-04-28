e6fc47f6d28ef5760b41142d454d21112391d4dd
Aleksi Kauppila <aleksi.kauppila@gmail.com>
2019-04-28 15:00:07 +0300

    Implement PDOUserRepository.getById()

--- app/src/Repository/PDOUserRepository.php
+++ app/src/Repository/PDOUserRepository.php
@@ -18,9 +18,18 @@ class PDOUserRepository implements UserRepository
         $this->pdo = $pdoFactory->build();
     }
 
+    /**
+     * @param UserId $userId
+     * @return User
+     * @throws NotFoundException
+     */
     public function getById(UserId $userId): User
     {
-        // TODO: Implement getById() method.
+        try {
+            return $this->getWhere('id=:id', ['id' => $userId->__toString()]);
+        } catch (NotFoundException $exception) {
+            throw new NotFoundException(sprintf('User not found with id %s', (string) $userId));
+        }
     }
 
     /**
@@ -30,24 +39,11 @@ class PDOUserRepository implements UserRepository
      */
     public function getByEmailAddress(EmailAddress $emailAddress): User
     {
-        $sql = <<<SQL
-SELECT "user".*,
-       application.id as app_id,
-       application.name as app_name,
-       application.site as app_siteurl,
-       application.secretkey as app_secretkey
-FROM "user"
-    JOIN application ON "user".app_id = application.id
-WHERE email=:email;
-SQL;
-        $statement = $this->pdo->prepare($sql);
-        $statement->execute(['email' => $emailAddress->__toString()]);
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        if (count($result) > 0) {
-            $user = $result[0];
-            return User::fromArray($user);
+        try {
+            return $this->getWhere('email=:email', ['email' => $emailAddress->__toString()]);
+        } catch (NotFoundException $exception) {
+            throw new NotFoundException(sprintf('User not found with email %s', (string) $emailAddress));
         }
-        throw new NotFoundException(sprintf('User not found with email %s', (string) $emailAddress));
     }
 
     public function store(User $user): void
@@ -63,5 +59,33 @@ SQL;
         $statement = $this->pdo->prepare($sql);
         $statement->execute(['email' => $user->email()->__toString()]);
         return $statement->rowCount() > 0;
+    }
+
+    /**
+     * @param string $whereClause
+     * @param array $parameters
+     * @return User
+     * @throws NotFoundException
+     */
+    private function getWhere(string $whereClause, array $parameters): User
+    {
+        $sql = <<<SQL
+SELECT "user".*,
+       application.id as app_id,
+       application.name as app_name,
+       application.site as app_siteurl,
+       application.secretkey as app_secretkey
+FROM "user"
+    JOIN application ON "user".app_id = application.id
+SQL;
+        $sql .= sprintf(' WHERE %s;', $whereClause);
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute($parameters);
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        if (count($result) > 0) {
+            $user = $result[0];
+            return User::fromArray($user);
+        }
+        throw new NotFoundException('User not found with');
     }
 }
