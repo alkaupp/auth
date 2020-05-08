65048ac830c7aec25f1860b149b411a39714650d
Aleksi Kauppila <aleksi.kauppila@gmail.com>
2020-05-08 12:57:53 +0300

    Fix code styles in PDOFactory.php

--- app/src/Repository/PDOFactory.php
+++ app/src/Repository/PDOFactory.php
@@ -12,12 +12,12 @@ class PDOFactory
         $db = parse_url(getenv('DATABASE_URL'));
 
         return new PDO('pgsql:' . sprintf(
-                'host=%s;port=%s;user=%s;password=%s;dbname=%s',
-                $db['host'],
-                $db['port'],
-                $db['user'],
-                $db['pass'],
-                ltrim($db['path'], '/')
-            ));
+            'host=%s;port=%s;user=%s;password=%s;dbname=%s',
+            $db['host'],
+            $db['port'],
+            $db['user'],
+            $db['pass'],
+            ltrim($db['path'], '/')
+        ));
     }
 }
